2642caca1d50b1a33b17d75049c58f00c817962e
imishikawa <iracema.ishikawa@gmail.com>
2024-06-17 17:39:55 -0300

    commit 4

--- app.py
+++ app.py
@@ -15,21 +15,31 @@ def exibir_opcoes():
     print('3. Ativar restaurantes')
     print('4. Sair\n')
 
+def finalizar_app():
+            print('Finalizando o app')
+
+def opcao_invalida():
+        print('Opção Inválida!\n')
+        input('Digite uma tecla para voltar ao menu principal')
+        main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: ')) # opcao_escolhida = int(opcao_escolhida)
-
-    def finalizar_app():
-        print('Finalizando o app')
-
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurantes')
-    else:
-        finalizar_app()
-
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+        
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2: 
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except: 
+        opcao_invalida()
 
 # Trecho de código suprimido
 
@@ -37,6 +47,6 @@ def main():
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
-    
+        
 if __name__ == '__main__':
     main()
